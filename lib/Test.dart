5a426aed05a8355d24ee306e59e439f5318f770a
osama450 <osama.kaboo55@gmail.com>
2022-02-13 18:34:24 +0200

    fix ui

--- lib/Test.dart
+++ lib/Test.dart
@@ -1,95 +1,96 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:xo_test_design/color.dart' as colors;
+
 class TestFile extends StatefulWidget {
   const TestFile({Key? key}) : super(key: key);
   @override
   State<TestFile> createState() => _TestFileState();
 }
-class _TestFileState extends State<TestFile>
-{
+
+class _TestFileState extends State<TestFile> {
   @override
-  void initState()
-  {
+  void initState() {
     super.initState();
   }
+
   @override
-  Widget build(BuildContext context)
-  {
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     double boardWidth = MediaQuery.of(context).size.width;
     return Scaffold(
-      backgroundColor:Colors.lightBlue,
-      body:buildBody(context, boardWidth),
+      backgroundColor: Colors.lightBlue,
+      body: buildBody(context, boardWidth, size),
     );
   }
-  Widget buildBody(context,boardWidth)=>Column(
-    mainAxisAlignment: MainAxisAlignment.center,
-    crossAxisAlignment: CrossAxisAlignment.center,
-    children:
-    [
-      Padding(
-        padding: const EdgeInsets.only(top: 20),
-        child: Text(
-          "It's turn".toUpperCase(),
-          style: TextStyle(
-            color: colors.secondColor,
-            fontSize: 45.0,
-            fontWeight: FontWeight.bold,
+
+  Widget buildBody(context, boardWidth, size) => Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Padding(
+            padding: EdgeInsets.only(top: 20),
+            child: Text(
+              "It's turn".toUpperCase(),
+              style: TextStyle(
+                color: colors.secondColor,
+                fontSize: 45.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
           ),
-        ),
-      ),
-      const SizedBox(height: 10.0,),
-      Container(
-        height: boardWidth,
-        width: boardWidth,
-        child: GridView.count(
-          crossAxisCount: 3,
-          padding:const EdgeInsets.all(16.0),
-          mainAxisSpacing: 8.0,
-          crossAxisSpacing: 8.0,
-          children: List.generate(
-              9,
-                  (index){
+          const SizedBox(
+            height: 10.0,
+          ),
+          SizedBox(
+            height: MediaQuery.of(context).size.height * 0.5,
+            width: boardWidth,
+            child: GridView.builder(
+              physics: NeverScrollableScrollPhysics(),
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  mainAxisExtent: MediaQuery.of(context).size.height * 0.13,
+                  crossAxisCount: 3,
+                  mainAxisSpacing: 10,
+                  crossAxisSpacing: 10),
+              itemCount: 9,
+              padding: EdgeInsets.all(16.0),
+              itemBuilder: (ctx, int i) {
                 return InkWell(
-                  onTap:()
-                  {
+                  onTap: () {
                     //we need add value and toggle player X , O
                     //and we need apply click when field is empty
                   },
                   child: Container(
-                    width: 100,
-                    height: 100,
                     decoration: BoxDecoration(
                       color: colors.primaryColor,
                       borderRadius: BorderRadius.circular(16.0),
                     ),
                     child: Center(
                       child: Text(
-                      'X',
+                        'X',
                       ),
                     ),
                   ),
                 );
-              }
+              },
+            ),
           ),
-        ),
-      ),
-      // Start Create button Reload
-      //const SizedBox(height: 25.0,),
-      Text('result', style: const TextStyle(color: Colors.white,fontSize: 45.0),),
-      ElevatedButton.icon(
-        onPressed: ()
-        {
-
-        },
-        icon:const Icon(Icons.replay,),
-        label:const Text(
-          'New Game',
-          style: TextStyle(fontSize: 16.0,fontWeight: FontWeight.bold),
-        ),
-      ),
-      // End Create button Reload
-    ],
-
-  );
+          // Start Create button Reload
+          //const SizedBox(height: 25.0,),
+          const Text(
+            'result',
+            style: TextStyle(color: Colors.white, fontSize: 45.0),
+          ),
+          ElevatedButton.icon(
+            onPressed: () {},
+            icon: const Icon(
+              Icons.replay,
+            ),
+            label: const Text(
+              'New Game',
+              style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold),
+            ),
+          ),
+          // End Create button Reload
+        ],
+      );
 }
